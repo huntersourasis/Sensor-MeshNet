af7b17e97870e62227ccfc851213d476d21da878
Sourasis Maity <128131302+huntersourasis@users.noreply.github.com>
2026-03-11 14:08:47 +0530

    Refactor packet handling and LED control
    Updated packet structure to include TTL and adjusted handling of incoming data based on packet size. Enhanced LED control and modified initialization message.

--- bridge_node_esp8266.ino
+++ bridge_node_esp8266.ino
@@ -1,12 +1,14 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+#include <user_interface.h> 
 
-#define STATUS_LED 2 // Onboard LED
+#define STATUS_LED 2 
 
 uint8_t broadcastAddress[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 
-typedef struct {
-  char json[200];
+typedef struct __attribute__((packed)) {
+  uint8_t ttl;      
+  char json[250];    
 } Packet;
 
 Packet incoming;
@@ -18,17 +20,35 @@ void OnDataRecv(uint8_t *mac, uint8_t *data, uint8_t len) {
   digitalWrite(STATUS_LED, LOW);
   ledOffTime = millis() + blinkDuration;
 
-  memcpy(&incoming, data, sizeof(incoming));
+  if (len == 250) {
+    incoming.ttl = 5;
+    memcpy(incoming.json, data, 250);
+  } 
+  else if (len == sizeof(Packet)) {
+    memcpy(&incoming, data, sizeof(incoming));
+  } 
+  else {
+    Serial.print("Dropped: Invalid packet size. Received ");
+    Serial.println(len);
+    return;
+  }
 
-  Serial.print("Received & Rebroadcasting: ");
+  if (incoming.ttl <= 1) {
+    return;
+  }
+
+  
+  incoming.ttl--; 
+
+  
   Serial.println(incoming.json);
-
+  
   esp_now_send(broadcastAddress, (uint8_t*)&incoming, sizeof(incoming));
 }
 
 void updateLED() {
   if (millis() >= ledOffTime && digitalRead(STATUS_LED) == LOW) {
-    digitalWrite(STATUS_LED, HIGH);
+    digitalWrite(STATUS_LED, HIGH); 
   }
 }
 
@@ -36,10 +56,12 @@ void setup() {
   Serial.begin(115200);
   
   pinMode(STATUS_LED, OUTPUT);
-  digitalWrite(STATUS_LED, HIGH);
+  digitalWrite(STATUS_LED, HIGH); 
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
+
+  wifi_set_channel(1);
 
   if (esp_now_init() != 0) {
     Serial.println("ESP-NOW init failed");
@@ -50,15 +72,9 @@ void setup() {
   esp_now_register_recv_cb(OnDataRecv);
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
 
-  Serial.println("Bridge node initialized.");
+  Serial.println("Bridge node initialized...");
 }
 
 void loop() {
   updateLED();
-  
-  static unsigned long lastHeartbeat = 0;
-  if (millis() - lastHeartbeat > 10000) {
-    Serial.println("Status: Waiting for packets...");
-    lastHeartbeat = millis();
-  }
 }
